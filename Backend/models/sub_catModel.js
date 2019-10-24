9d27bcf3dd8169382ceba00ac5c670e26ef66750
sambhav jain <sambhavjain9138@gmail.com>
2019-10-24 15:00:16 +0530

    subcategory model updated

--- Backend/models/sub_catModel.js
+++ Backend/models/sub_catModel.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 var Sub_cat = new mongoose.Schema({
+    cat_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cat"
+    },
     name: String,
     quantity_type: String
 });
